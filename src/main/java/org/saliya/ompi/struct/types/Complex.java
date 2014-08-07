372b9c801d9d79ccba2a2b2a7202a641434ac42b
Saliya Ekanayake <esaliya@gmail.com>
2014-08-07 16:28:53 -0400

    Trying out OMPI struct type

---
+++ src/main/java/org/saliya/ompi/struct/types/Complex.java
@@ -0,0 +1,11 @@
+package org.saliya.ompi.struct.types;
+
+import mpi.Struct;
+
+public class Complex extends Struct {
+
+    @Override
+    protected Data newData() {
+        return null;
+    }
+}
