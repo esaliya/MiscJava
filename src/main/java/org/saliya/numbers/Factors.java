1b49b97ca22fcf6a158c1dcf074ece5c64f74939
Saliya Ekanayake <esaliya@gmail.com>
2014-12-18 23:39:34 -0500

    adding factors (just started)

---
+++ src/main/java/org/saliya/numbers/Factors.java
@@ -0,0 +1,12 @@
+package org.saliya.numbers;
+
+/**
+ * Finds two factors, p1 and p2, of the given number such that max(1/p1, 1/p2) is minimum
+ */
+public class Factors {
+    public static void main(String[] args) {
+        int num = 256;
+
+        int [] knownPrimes = new int[]{2,3,5,7,9};
+    }
+}
