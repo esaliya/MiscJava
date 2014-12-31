e55a275fc908c9cef9399529d80791c683cb86aa
Saliya Ekanayake <esaliya@gmail.com>
2014-12-31 03:16:42 -0500

    adding weights 1 writer

--- src/main/java/org/saliya/binary/WeightsWriter.java
+++ src/main/java/org/saliya/binary/WeightsWriter.java
@@ -1,30 +1,31 @@
 package org.saliya.binary;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.SeekableByteChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class WeightsWriter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int rows = Integer.parseInt(args[0]);
-        Path path = Paths.get("weights.bin");
-        try (FileChannel fc = (FileChannel) Files.newByteChannel(path, StandardOpenOption.WRITE)) {
-            MappedByteBuffer mappedBytes = fc.map(FileChannel.MapMode.READ_WRITE, 0,
-                                                  rows * rows * Short.BYTES);
-            mappedBytes.order(ByteOrder.BIG_ENDIAN);
-            if (rows < Math.sqrt(Integer.MAX_VALUE)){
-                int count = rows * rows;
-                for (int i = 0; i < count; ++i){
-                    mappedBytes.putShort((short)1);
-                }
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
+        String file = "weights.bin";
+
+        SeekableByteChannel bc = Files.newByteChannel(Paths.get(file), StandardOpenOption.CREATE);
+        ByteBuffer buffer = ByteBuffer.allocateDirect(rows * Short.BYTES);
+        buffer.position(0);
+        for (int i = 0; i < rows; ++i){
+            buffer.putShort((short)1);
         }
+        for (int i = 0; i < rows; ++i) {
+            buffer.position(0);
+            bc.write(buffer);
+        }
+        bc.close();
     }
 }
